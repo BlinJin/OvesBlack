8f0a5cd4db03b699a58e7de24460d38aaae643c5
Imax <imax13@mail.ru>
2016-07-03 23:45:12 +0300

    Added new galleries for textile and wallpaper

--- js/controllers/app.js
+++ js/controllers/app.js
@@ -18,14 +18,20 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
                 $scope.items = ["A", "List", "Of", "Items"];
             }
         })
-        .state('event', {
-            url: "/event",
-            templateUrl: "pages/events.html"
+        .state('textile', {
+            url: "/textile",
+            templateUrl: "pages/textile.html",
+            controller:"GalleryControllerTextile"
         })
         .state('gallery-blind', {
             url: "/gallery-blind",
             templateUrl: "pages/gallery-blind.html",
             controller:"GalleryControllerBlinds"
+        })
+        .state('wallpaper', {
+            url: "/wallpaper",
+            templateUrl: "pages/wallpaper.html",
+            controller:"GalleryControllerWallpaper"
         })
         .state('gallery-fire', {
             url: "/gallery-fire",
@@ -35,11 +41,6 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
         .state('contacts', {
             url: "/contacts",
             templateUrl: "pages/contacts.html"
-        })
-        .state('calldesign', {
-            url: "/calldesign",
-            templateUrl: "pages/backform.html",
-            controller:"BackFormController"
         });
 });
 
@@ -171,6 +172,111 @@ myApp.controller('GalleryControllerFire', ['$scope', '$http', '$state', function
 
 
 }]);
+
+myApp.controller('GalleryControllerWallpaper', ['$scope', '$http', '$state', function($scope, $http, $state) {
+    $scope.state = $state;
+    $scope.filter = {
+        "wallpaperFilter":[
+            {"all" : true},
+            {"child" : false},
+            {"flower" : false},
+            {"geometry" : false},
+            {"modern" : false},
+            {"other" : false},
+            {"classic" : false}
+        ]
+    };
+    $scope.categoryWallpaper={};
+    $http.get('./json/gallery/wallpaper/gallery_wallpaper.json').success(function (data) {
+        $scope.categoryWallpaper  = data;
+        //alert(JSON.stringify($scope.categoryWallpaper, null, 4));
+    });
+
+    $scope.filterChange = function(param)
+    {
+        if (param == 'all')
+        {
+            $scope.filter.wallpaperFilter.all = true;
+            $scope.filter.wallpaperFilter.child = true;
+            $scope.filter.wallpaperFilter.flower = true;
+            $scope.filter.wallpaperFilter.geometry = true;
+            $scope.filter.wallpaperFilter.modern = true;
+            $scope.filter.wallpaperFilter.other = true;
+            $scope.filter.wallpaperFilter.classic = true;
+        }
+        else {
+            $scope.filter.wallpaperFilter.all = false;
+            $scope.filter.wallpaperFilter.child = false;
+            $scope.filter.wallpaperFilter.flower = false;
+            $scope.filter.wallpaperFilter.geometry = false;
+            $scope.filter.wallpaperFilter.modern = false;
+            $scope.filter.wallpaperFilter.other = false;
+            $scope.filter.wallpaperFilter.classic = false;
+            $scope.filter.wallpaperFilter[param] = !$scope.filter.wallpaperFilter[param];
+        }
+    };
+    $scope.filterChange('all');
+
+    $scope.filterCategory = function(category)
+    {
+        return $scope.filter.wallpaperFilter[category];
+    };
+    
+
+}]);
+
+myApp.controller('GalleryControllerTextile', ['$scope', '$http', '$state', function($scope, $http, $state) {
+    $scope.state = $state;
+    $scope.filter = {
+        "textileFilter":[
+            {"all" : true},
+            {"child" : false},
+            {"flower" : false},
+            {"geometry" : false},
+            {"modern" : false},
+            {"other" : false},
+            {"classic" : false}
+        ]
+    };
+    $scope.categoryTextile={};
+    $http.get('./json/gallery/textile/gallery_textile.json').success(function (data) {
+        $scope.categoryTextile  = data;
+        //alert(JSON.stringify($scope.categoryWallpaper, null, 4));
+    });
+
+    $scope.filterChange = function(param)
+    {
+        if (param == 'all')
+        {
+            $scope.filter.textileFilter.all = true;
+            $scope.filter.textileFilter.child = true;
+            $scope.filter.textileFilter.flower = true;
+            $scope.filter.textileFilter.geometry = true;
+            $scope.filter.textileFilter.modern = true;
+            $scope.filter.textileFilter.other = true;
+            $scope.filter.textileFilter.classic = true;
+        }
+        else {
+            $scope.filter.textileFilter.all = false;
+            $scope.filter.textileFilter.child = false;
+            $scope.filter.textileFilter.flower = false;
+            $scope.filter.textileFilter.geometry = false;
+            $scope.filter.textileFilter.modern = false;
+            $scope.filter.textileFilter.other = false;
+            $scope.filter.textileFilter.classic = false;
+            $scope.filter.textileFilter[param] = !$scope.filter.textileFilter[param];
+        }
+    };
+    $scope.filterChange('all');
+
+    $scope.filterCategory = function(category)
+    {
+        return $scope.filter.textileFilter[category];
+    };
+
+
+}]);
+
 myApp.controller('BackFormController', ['$scope', '$http', '$state', function($scope, $http, $state) {
     $scope.sendEmail = function()
     {
